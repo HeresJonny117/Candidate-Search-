bd069fcbf8c2a518e644e16a77de3701a0338930
Jonathan Hegarty <jhegarty968@gmail.com>
2024-11-06 20:01:57 -0500

    added Nav.tsx begin code, added cadidate.interface.tsx begin code

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,20 @@
-const Nav = () => {
+import React from 'react';
+import { Link } from 'react-router-dom';
+ 
   // TODO: Add necessary code to display the navigation bar and link between the pages
+const Nav: React.FC =() => {
   return (
-    <div>Nav</div>
-  )
+    <nav>
+      <ul>
+        <li>
+          <Link to="/search">Candidate Search</Link>
+        </li>
+        <li>
+          <Link to="/saved">Saved Candidates</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,17 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+import { Candidate } from './path/to/Candidate';
+
+export async function CandidateSearch(): Promise<Candidate> {
+  const response = await fetch('searchGithubUser');
+  const data = await response.json();
+
+  return {
+    name: data.name,
+    username: data.login,
+    location: data.location,
+    avatar: data.avatar_url,
+    email: data.email || null,
+    html_url: data.html_url,
+    company: data.company || null,
+  };
+}
